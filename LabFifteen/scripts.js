953d9251f38eded6793c6f38c4ac678b4e54ded6
mcclowrycolin <mcclowcj0091@uwec.edu>
2023-12-05 20:22:46 -0600

    Complete Lab Fifteen

--- LabFifteen/scripts.js
+++ LabFifteen/scripts.js
@@ -22,3 +22,24 @@ document.addEventListener('DOMContentLoaded', function(){
 		});
 	}
 });
+document.addEventListener('DOMContentLoaded', function(){
+	document.querySelectorAll('.cloner').forEach(function(cloner){
+		cloner.querySelector('.add-button').addEventListener('click', function(e){
+			cloneRow(cloner);
+		});
+		cloneRow(cloner);
+	});
+});
+
+function cloneRow(cloner){
+	let container = cloner.querySelector('.container');
+	let template = cloner.querySelector('template');
+	let clon = template.content.cloneNode(true);
+	container.appendChild(clon);
+}
+
+function removeRow(e){
+	let button = e.target;
+	let row = button.closest('.row');
+	row.remove();
+}
